24f67cdf01dde99b913dda10e102c1303f6cd377
Catherine Tran & Kyle Smith <student@epicodus.com>
2014-09-09 12:19:38 -0700

    create show,new and create methods with corresponding html views, including flash message when create is successful.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -4,5 +4,23 @@ class LessonsController < ApplicationController
     render('lessons/index.html.erb')
   end
 
+  def show
+    @lesson = Lesson.find(params[:id])
+    render('lessons/show.html.erb')
+  end
 
+  def new
+    render('lessons/new.html.erb')
+  end
+
+  def create
+    @lesson = Lesson.create(:name => params[:name],
+                            :description => params[:description])
+    if @lesson.save
+      flash[:notice] = "You successfully created a lesson!"
+      redirect_to("/lessons/#{@lesson.id}")
+    else
+      render('lessons/new.html.erb')
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   match('lessons', {:via => :get, :to => 'lessons#index'})
+  match('lessons', {:via => :post, :to => 'lessons#create'})
+  match('lessons/new',{:via => :get, :to => 'lessons#new'})
+  match('lessons/:id', {:via => :get, :to => 'lessons#show'})
+
 end
